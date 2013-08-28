29d31d906c77b9ecdd1090282a8d517b3b8cb398
Christopher Lamm <chris@theantichris.com>
2013-08-28 17:36:00 -0400

    Start of plugin. Structure of the Singleton.

---
+++ wordpress-plugin-framework.php
@@ -0,0 +1,46 @@
+<?php
+
+/*
+  Plugin Name: WordPress Plugin Framework
+  Plugin URI: https://github.com/theantichris/WordPress-Plugin-Framework
+  Description: My own framework for making the WordPress plugins the way I do.
+  Version: 1.0.0
+  Author: Christopher Lamm
+  Author URI: http://www.theantichris.com
+  License: GPL V2
+ */
+
+/**
+ * Class WordPress_Plugin_Framework
+ *
+ * @since 1.0.0
+ */
+class WordPress_Plugin_Framework {
+	/** @var null|WordPress_Plugin_Framework Refers to a single instance of this class. */
+	private static $instance = null;
+
+	/**
+	 * Creates or returns an instance of this class.
+	 *
+	 * @return WordPress_Plugin_Framework A single instance of this class.
+	 */
+	public static function get_instance() {
+		// If an instance hasn't been create and set to $instance create an instance and set it to $instance.
+		if ( null == self::$instance ) {
+			self::$instance = new self;
+		}
+
+		return self::$instance;
+	}
+
+	/**
+	 * Initializes the plugin by setting localization, filters, and administration functions.
+	 *
+	 * Add this code as needed.
+	 */
+	private function __construct() {
+
+	}
+}
+
+WordPress_Plugin_Framework::get_instance();

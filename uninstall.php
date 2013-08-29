57d0f2a7cfbb1999d114f9c6818890d151a1ac9a
Christopher Lamm <chris@theantichris.com>
2013-08-29 15:21:19 -0400

    Uninstall file.

---
+++ uninstall.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * The code in this file runs when a plugin is uninstalled from the WordPress dashboard.
+ *
+ * @since 1.0.0
+ */
+
+/* If uninstall is not called from WordPress exit. */
+if ( !defined( 'WP_UNINSTALL_PLUGIN' ) ) {
+	exit ();
+}
+
+/* Place uninstall code below here. */

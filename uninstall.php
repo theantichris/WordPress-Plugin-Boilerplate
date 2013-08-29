d8c46f6120c2ee2c0adc1b21addc6bb01c011d07
Christopher Lamm <chris@theantichris.com>
2013-08-29 16:43:21 -0400

    DocBlock.

--- uninstall.php
+++ uninstall.php
@@ -3,7 +3,11 @@
 /**
  * The code in this file runs when a plugin is uninstalled from the WordPress dashboard.
  *
- * @since 1.0.0
+ * @author Christopher Lamm chris@theantichris.com
+ * @copyright 2013 Christopher Lamm
+ * @license GNU General Public License, version 3
+ * @license http://opensource.org/licenses/gpl-license.php GNU Public License
+ * @link http://www.theantichris.com
  */
 
 /* If uninstall is not called from WordPress exit. */

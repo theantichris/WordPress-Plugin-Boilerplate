5846fb1434f379d4286dbc9fd051cc5f75906b24
Christopher Lamm <chris@theantichris.com>
2013-08-31 09:18:27 -0400

    File for the custom post type class.

---
+++ inc/custom-post-type.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: Christopher
+ * Date: 8/31/13
+ * Time: 9:17 AM
+ * To change this template use File | Settings | File Templates.
+ */
+
+class CustomPostType {
+
+}

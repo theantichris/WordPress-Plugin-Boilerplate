d47acf52b42c4b098f28bfd78d52637298248792
Christopher Lamm <chris@theantichris.com>
2013-09-02 17:29:21 -0400

    Added labels to the custom post type creation.

--- inc/custom-post-type.php
+++ inc/custom-post-type.php
@@ -10,17 +10,48 @@
  */
 
 class CustomPostType {
-	/** @var  string User readable name of the post type. */
+	/** @var  string User readable name of the post type. Must be plural. */
 	private $post_type_name;
 	/** @var  string WordPress ID of the post type. */
 	private $post_type_slug;
 	/** @var  array Arguments for the register_post_type() function. */
-	private $post_type_args;
+	private $post_type_args = array();
+	/** @var array Labels for the post type. */
+	private $post_type_labels = array();
 
+	/**
+	 * Class constructor.
+	 *
+	 * @since 2.0.0
+	 *
+	 * @param string $post_type_name User readable name of the post type. Must be plural.
+	 */
 	function __construct( $post_type_name ) {
 		$this->post_type_name = $post_type_name;
 		$this->post_type_slug = str_replace( ' ', '-', strtolower( $post_type_name ) );
-		$this->post_type_args = array( 'public' => true, 'label' => $this->post_type_name );
+
+		$singular = WordPress_Plugin_Framework::make_singular( $this->post_type_name );
+
+		$this->post_type_labels = array(
+			'name' => $this->post_type_name,
+			'singular_name' => $singular,
+			'add_new' => 'Add New',
+			'add_new_item' => 'Add New ' . $singular,
+			'edit_item' => 'Edit ' . $singular,
+			'new_item' => 'New ' . $singular,
+			'all_items' => 'All ' . $this->post_type_name,
+			'view_item' => 'View ' . $singular,
+			'search_items' => 'Search ' . $this->post_type_name,
+			'not_found' => 'No ' . strtolower( $this->post_type_name ) . ' found',
+			'not_found_in_trash' => 'No ' . strtolower( $this->post_type_name ) . ' found in Trash',
+			'parent_item_colon' => '',
+			'menu_name' => $this->post_type_name
+		);
+
+		$this->post_type_args = array(
+			'labels' => $this->post_type_labels,
+			'public' => true
+		);
 
 		add_action( 'init', array( $this, 'register_custom_post_type' ) );
 	}
@@ -32,7 +63,7 @@ class CustomPostType {
 	 *
 	 * @return void
 	 */
-	public function register_custom_post_type() {
+	public  function register_custom_post_type() {
 		register_post_type( $this->post_type_slug, $this->post_type_args );
 	}
 }

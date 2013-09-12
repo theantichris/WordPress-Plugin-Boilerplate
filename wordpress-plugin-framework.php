f8b33720e2a05d9edd2415aba790055922965152
Christopher Lamm <chris@theantichris.com>
2013-09-12 15:52:28 -0400

    Adding includes. Updating version number.

--- wordpress-plugin-framework.php
+++ wordpress-plugin-framework.php
@@ -4,7 +4,7 @@
   Plugin Name: WordPress Plugin Framework
   Plugin URI: https://github.com/theantichris/WordPress-Plugin-Framework
   Description: My own framework for making WordPress plugins the way I do.
-  Version: 3.2.0
+  Version: 4.0.0
   Author: Christopher Lamm
   Author URI: http://www.theantichris.com
   License: GPL V2
@@ -62,6 +62,8 @@ class WordPress_Plugin_Framework {
 	private function __construct() {
 		/* Includes */
 		include_once 'inc/custom-post-type.php';
+		include_once 'inc/page.php';
+		include_once 'inc/menu-page.php';
 		include_once 'inc/taxonomy.php';
 
 		/* Set properties. */

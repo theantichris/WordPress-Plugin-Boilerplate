bbc2f7e0fae994a1d2b9169fa5b82ddf7a9f91f5
Christopher Lamm <chris@theantichris.com>
2013-09-12 16:19:11 -0400

    The abstraction won't work without these properties in the abstract class.

--- inc/menu-page.php
+++ inc/menu-page.php
@@ -10,41 +10,6 @@
  */
 
 class Menu_Page extends Page {
-	/** @var string|null The URL to the icon to be used for the menu item. */
-	private $icon_url = null;
-	/** @var integer|null The position in the menu this page should appear. */
-	private $position = null;
-
-	/**
-	 * Sets $this->icon_url.
-	 *
-	 * @since 4.0.0
-	 *
-	 * @param string|null $icon_url
-	 *
-	 * @return void
-	 */
-	public function set_icon_url( $icon_url = null ) {
-		if ( empty( $icon_url ) ) {
-			$this->icon_url = $icon_url;
-		}
-	}
-
-	/**
-	 * Sets $this->position.
-	 *
-	 * @since 4.0.0
-	 *
-	 * @param null $position
-	 *
-	 * @return void
-	 */
-	public function set_position( $position = null ) {
-		if ( !empty( $position ) ) {
-			$this->position = $position;
-		}
-	}
-
 	/**
 	 * Registers the page with WordPress.
 	 *
--- inc/page.php
+++ inc/page.php
@@ -20,18 +20,24 @@ abstract class Page {
 	private $page_slug;
 	/** @var string The capability required for the menu item to be displayed to the user. */
 	private $capability = 'manage_options';
+	/** @var string|null The URL to the icon to be used for the menu item. */
+	private $icon_url = null;
+	/** @var integer|null The position in the menu this page should appear. */
+	private $position = null;
 
 	/**
 	 * Class constructor.
 	 *
 	 * @since 4.0.0
 	 *
-	 * @param string $page_title User readable title for the page and menu item.
-	 * @param string $capability The capability required for the menu item to be displayed to the user.
+	 * @param string      $page_title User readable title for the page and menu item.
+	 * @param string      $capability The capability required for the menu item to be displayed to the user.     *
+	 * @param string|null $icon_url   The URL to the icon to be used for the menu item.
+	 * @param string|null $position   The position in the menu this page should appear.
 	 *
 	 * @return Page
 	 */
-	public function __construct( $page_title, $capability = null ) {
+	public function __construct( $page_title, $capability = null, $icon_url = null, $position = null ) {
 		$this->page_title = $page_title;
 		$this->page_slug  = WordPress_Plugin_Framework::make_slug( $page_title );
 

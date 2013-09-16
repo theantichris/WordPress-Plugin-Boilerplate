1562da74f91363c8e7a4e992b8d7e374c5fc700a
Christopher Lamm <chris@theantichris.com>
2013-09-16 11:55:15 -0400

    Class for adding a sub menu to the Settings section of the Dashboard.

---
+++ inc/options-page.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * A class for creating an options sub menu page in WordPress.
+ *
+ * @author    Christopher Lamm chris@theantichris.com
+ * @copyright 2013 Christopher Lamm
+ * @license   GNU General Public License, version 3
+ * @license   http://opensource.org/licenses/gpl-license.php GNU Public License
+ * @link      http://www.theantichris.com
+ *
+ * @package    WordPress
+ * @subpackage WordPressPluginFramework
+ *
+ * @since 5.0.0
+ */
+
+/*
+ * TODO: Replace "WordPressPluginFramework" with "PluginName".
+ */
+
+class Options_Page extends Page {
+	/**
+	 * Add the page with WordPress.
+	 *
+	 * @since 4.0.0
+	 *
+	 * @return void
+	 */
+	public function add_page() {
+		add_options_page( $this->page_title, $this->page_title, $this->capability, $this->page_slug, array( $this, 'display_page' ) );
+	}
+
+	/**
+	 * Removes a page from WordPress.
+	 *
+	 * @since 4.0.0
+	 *
+	 * @return void
+	 */
+	public function remove_page() {
+		remove_menu_page( $this->page_slug );
+	}
+}

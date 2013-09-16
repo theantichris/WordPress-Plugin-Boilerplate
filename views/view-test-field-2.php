df97bcf711f0485b172706cf88847fa84d4fd9e6
Christopher Lamm <chris@theantichris.com>
2013-09-16 17:36:36 -0400

    Need to echo get options.

--- views/view-test-field-2.php
+++ views/view-test-field-2.php
@@ -1,1 +1,1 @@
-<label><input id="<?php echo esc_attr( $id ); ?>" name="<?php echo esc_attr( $id ); ?>" type="text" value="<?php get_option( $id ); ?>" /></label>
+<label><input id="<?php echo esc_attr( $id ); ?>" name="<?php echo esc_attr( $id ); ?>" type="text" value="<?php echo get_option( $id ); ?>" /></label>
